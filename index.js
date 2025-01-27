0087bd9d85553210a7a8c66fa986aec820516c97
unknown <jan.kouril@infis.cz>
2025-01-27 08:41:39 +0100

    idk

---
+++ index.js
@@ -0,0 +1,14 @@
+alert("na výběr z destinací je Japonsko, Řecko a Itálie")
+var destinace=(prompt("zadejte vaší vybranou destinaci"))
+if(destinace=="Řecko"){
+    alert("Řecko je super volba")
+}
+else if(destinace=="Japonsko"){
+    alert("Japonsko je velice krásné")
+}
+else if(destinace=="Itálie"){
+    alert("Itálie je velmi poklidná")
+}
+else{
+    alert("takovou destinaci zde nenaleznete")
+}
